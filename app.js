1b4761485dd30baf2af502b92dda5e6cc39287a0
Michael s <msullivan255@gmail.com>
2023-08-19 16:51:42 -0500

    graphql initialized

--- app.js
+++ app.js
@@ -1,12 +1,42 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const { graphqlHTTP } = require('express-graphql');
+const { buildSchema } = require('graphql');
 
 const app = express();
 
 app.use(bodyParser.json());
 
-app.get('/', (req, res, next) => {
-    res.send('Hello World!');
-})
+app.use(
+  '/graphql',
+  graphqlHTTP({
+    schema: buildSchema(`
+      type RootQuery {
+        lifts: [String!]!
+      }
 
-app.listen(3000);
+      type RootMutation {
+        addLift(name: String): String
+      }
+
+      schema {
+        query: RootQuery
+        mutation: RootMutation
+      }
+    `),
+    rootValue: {
+      lifts: () => {
+        return ['Bench Press', 'Squat', 'Deadlift'];
+      },
+      addLift: (args) => {
+        const liftName = args.name;
+        return liftName;
+      },
+    },
+    graphiql: true,
+  })
+);
+
+app.listen(3000, () => {
+  console.log('GraphQL server is running at http://localhost:3000/graphql');
+});
